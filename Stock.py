9d897837fb6b104ecc345d1e68e2e2c76c0e3035
lslszz <isidoramellado0704@gmail.com>
2025-10-15 01:06:41 -0300

    feat: completar clase stock

--- Stock.py
+++ Stock.py
@@ -5,17 +5,37 @@ class Stock:
         self.lista_ingredientes = []
 
     def agregar_ingrediente(self, ingrediente):
-        pass
+        for i in self.lista_ingredientes:
+            if i.nombre == ingrediente.nombre:
+                if hasattr(i, "unidad") and hasattr (ingrediente, "unidad"):
+                    if i.unidad == ingrediente.unidad:
+                        i.cantidad =str(int(i.cantidad) + int(ingrediente.cantidad))
+                    else:
+                        print( f"Las unidades de medida deben ser '{i.nombre}'({i.unidad } vs {ingrediente.unidad}) ")
+                        return
+                else:
+                    i.cantidad =str(int(i.cantidad) + int(ingrediente.cantidad))
+                    return
+                return       
+        self.lista_ingredientes.append(ingrediente)
 
     def eliminar_ingrediente(self, nombre_ingrediente):
-        pass    
+        for i in self.lista_ingredientes:   
+            if i.nombre == nombre_ingrediente:
+                self.lista_ingredientes.remove(i)
+                break
+ 
 
     def verificar_stock(self):
-        pass
+        return len(self.lista_ingredientes)
+
 
     def actualizar_stock(self, nombre_ingrediente, nueva_cantidad):
-        pass
+        for i in self.lista_ingredientes:
+            if i.nombre == nombre_ingrediente:
+                i.cantidad = nueva_cantidad
+                break
 
     def obtener_elementos_menu(self):
-        pass
+        return [i for i in self.lista_ingredientes]
 
