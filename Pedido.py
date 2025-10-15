67dfe93dde6634456d9ac7ebd85bdf487a34e13c
lslszz <isidoramellado0704@gmail.com>
2025-10-15 03:31:08 -0300

    feat: completar implementación de clase Pedido
    Se implementaron todos los metodos de la clase pedido

--- Pedido.py
+++ Pedido.py
@@ -4,13 +4,27 @@ class Pedido:
         self.menus = []  
 
     def agregar_menu(self, menu: CrearMenu):
-        pass
+        for m in self.menus:
+            if m.nombre ==menu.nombre:
+                m.cantidad += 1
+                return
+            
+        menu.cantidad=1
+        self.menus.append(menu)
 
     def eliminar_menu(self, nombre_menu: str):
-        pass
+        for i, m in enumerate(self.menus):
+            if m.nombre == nombre_menu:
+                self.menus.pop(i)
+                return True
+        return False
 
     def mostrar_pedido(self):
-        pass
+        for m in self.menus:
+            print (f"{m.nombre} x {m.cantidad} -${m.precio * m.cantidad} ")
 
     def calcular_total(self) -> float:
-        pass
+        total = 0
+        for menu in self.menus:
+            total += menu.precio * menu.cantidad
+        return total
